e67a741bb050ff0757199730138e87bc3e4291dc
lugabe <luis2014gbp@gmail.com>
2023-07-28 01:18:41 -0300

    Ajustes nas imagens

--- pages/controle/controle.php
+++ pages/controle/controle.php
@@ -1,1 +1,18 @@
-<h3>Controle</h3>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/styles.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
+<h3 id="h3-titulo-controle">Controle</h3>
+<p id="p-controle">Que tal ter o controle das <span> finanças</span>, <span> insumos</span> e <span> agendamentos</span> na palma da sua mão?</p>
+<h4 id="h4-constrole">Estamos em construção, volte mais tarde!</h4>
--- pages/home/home.php
+++ pages/home/home.php
@@ -1,1 +1,22 @@
-<h3>Página Principal!</h3>
+<!DOCTYPE html>
+<html lang="pt ">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/styles.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+<h3 id="h3-home">Sejá bem vindo ao Salão da Leila!</h3>
+<p id="p-home">O nosso site está em construção, mas logo logo o nosso site vai ficar uma belezura igual os seus cabelos!</p>
+<h5 id="h5-home">Enquanto isso, venha visitar o nosso salão de beleza e conheça o melhor salão da região!</h5>
+<div id="div-img">
+    <img id="home" src="./../../assets/logo-leila.jpg" alt="">
+</div>
